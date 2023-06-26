cf2ef0a006f61fdb7ce91f15169ab869959e6c91
Sergio Fernández <sergiofdezfernandez@gmail.com>
2023-06-27 01:51:18 +0200

    responsive form and converting forms to cards

--- components/app/register_property_form.tsx
+++ components/app/register_property_form.tsx
@@ -1,13 +1,18 @@
-import { Button, Form, Input, InputNumber, notification, message } from 'antd';
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  notification,
+  Col,
+  Row,
+  Card,
+} from "antd";
 import Title from "antd/es/typography/Title";
 import { AnyARecord } from "dns";
 import React from "react";
 import { IWeb3Context, useWeb3Context } from "../web3/Web3Context";
-import { JsonRpcError } from 'ethers';
-
-const tailLayout = {
-  wrapperCol: { offset: 14, span: 4 },
-};
+import { JsonRpcError } from "ethers";
 
 const RegisterPropertyForm: React.FC = () => {
   const {
@@ -30,9 +35,9 @@ const RegisterPropertyForm: React.FC = () => {
         message: "Registro de propiedad",
         description: "Propiedad registrada correctamente",
       });
-    } catch (error:any) {
-      notification.error({message:error.code, description:error.reason})
-      console.log(error)
+    } catch (error: any) {
+      notification.error({ message: error.code, description: error.reason });
+      console.log(error);
     }
   }
 
@@ -42,77 +47,81 @@ const RegisterPropertyForm: React.FC = () => {
   return (
     <Form
       name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600 }}
+      labelCol={{ span: 4 }}
       initialValues={{ remember: true }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-      <Title level={3}>Property registration</Title>
-      <Title level={4}>Porperty data</Title>
-      <Form.Item
-        label="postalAddress"
-        name="postalAddress"
-        rules={[
-          { required: true, message: "Please enter your postal address" },
-        ]}
+      <Title level={2}>Registrar propiedad</Title>
+      <Card
+        bordered={false}
+        actions={
+          [<Form.Item key={"submit"}>
+            <Button type="default" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>]
+        }
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="description"
-        name="description"
-        rules={[{ required: false }]}
-      >
-        <Input.TextArea />
-      </Form.Item>
-      <Form.Item
-        label="price"
-        name="price"
-        rules={[{ required: true, message: "Please enter the price" }]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        label="securityDeposit"
-        name="securityDeposit"
-        rules={[{ required: true, message: "Please enter a deposit" }]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Title level={4}>Landlord data</Title>
-      <Form.Item
-        label="landlordName"
-        name="landlordName"
-        rules={[{ required: true, message: "Please enter your landlord name" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="landlordEmail"
-        name="landlordEmail"
-        rules={[
-          { required: true, message: "Please enter your landlord email" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="landlordPhone"
-        name="landlordPhone"
-        rules={[
-          { required: true, message: "Please enter your landlord phone" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item {...tailLayout}>
-        <Button type="default" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
+        <Form.Item
+          label="postalAddress"
+          name="postalAddress"
+          rules={[
+            { required: true, message: "Please enter your postal address" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="description"
+          name="description"
+          rules={[{ required: false }]}
+        >
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item
+          label="price"
+          name="price"
+          rules={[{ required: true, message: "Please enter the price" }]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          label="securityDeposit"
+          name="securityDeposit"
+          rules={[{ required: true, message: "Please enter a deposit" }]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          label="landlordName"
+          name="landlordName"
+          rules={[
+            { required: true, message: "Please enter your landlord name" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="landlordEmail"
+          name="landlordEmail"
+          rules={[
+            { required: true, message: "Please enter your landlord email" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="landlordPhone"
+          name="landlordPhone"
+          rules={[
+            { required: true, message: "Please enter your landlord phone" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </Card>
     </Form>
   );
 };
